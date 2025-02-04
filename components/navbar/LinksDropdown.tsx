15f55e977904e7eb4863d9cf7c24c60f33cd9c0e
mateusz <mateusz@mateuszs-Mini.mynet>
2025-02-04 18:38:37 +0000

    add key to dropdown item

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -7,22 +7,24 @@ import {
 } from "@/components/ui/dropdown-menu";
 import {links} from "@/utils/links";
 import Link from "next/link";
-import { LuAlignLeft } from 'react-icons/lu';
-import { Button } from "../ui/button";
+import {LuAlignLeft} from "react-icons/lu";
+import {Button} from "../ui/button";
 
 function LinksDropdown() {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-            <Button variant='outline' className="flex gap-4 max-w-[100px]">
-               <LuAlignLeft className="w-6 h-6"/>
-            </Button>
-         </DropdownMenuTrigger>
-			<DropdownMenuContent className="w-40" align='start' sideOffset={10}>
+				<Button variant="outline" className="flex gap-4 max-w-[100px]">
+					<LuAlignLeft className="w-6 h-6" />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="w-40" align="start" sideOffset={10}>
 				{links.map((link) => {
 					return (
-						<DropdownMenuItem>
-							<Link href={link.href} className="capitalize">{link.label}</Link>
+						<DropdownMenuItem key={link.name}>
+							<Link href={link.href} className="capitalize">
+								{link.label}
+							</Link>
 						</DropdownMenuItem>
 					);
 				})}
