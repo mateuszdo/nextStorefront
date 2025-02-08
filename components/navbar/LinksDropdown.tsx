3d90ebf8ba7c8ca816fb41142b7f968b8ccde121
mateusz <dobrzynskimateusz@gmail.com>
2025-02-08 09:58:52 +0000

    add auth provider

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -9,6 +9,9 @@ import {links} from "@/utils/links";
 import Link from "next/link";
 import {LuAlignLeft} from "react-icons/lu";
 import {Button} from "../ui/button";
+import SignOutLink from "./SignOutLink";
+import UserIcon from "./UserIcon";
+import {SignInButton, SignUpButton, SignedIn, SignedOut} from "@clerk/nextjs";
 
 function LinksDropdown() {
 	return (
@@ -16,18 +19,36 @@ function LinksDropdown() {
 			<DropdownMenuTrigger asChild>
 				<Button variant="outline" className="flex gap-4 max-w-[100px]">
 					<LuAlignLeft className="w-6 h-6" />
+					<UserIcon />
 				</Button>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent className="w-40" align="start" sideOffset={10}>
-				{links.map((link) => {
-					return (
-						<DropdownMenuItem key={link.label}>
-							<Link href={link.href} className="capitalize">
-								{link.label}
+				<SignedOut>
+					<DropdownMenuItem>
+						<SignInButton mode="modal">
+							<button className="w-full text-left">Login</button>
+						</SignInButton>
+					</DropdownMenuItem>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>
+						<SignUpButton mode="modal">
+							<button className="w-full text-left">Register</button>
+						</SignUpButton>
+					</DropdownMenuItem>
+				</SignedOut>
+				<SignedIn>
+					{links.map((link) => {
+						return (
+							<Link href={link.href} className="capitalize" key={link.label}>
+								<DropdownMenuItem>{link.label}</DropdownMenuItem>
 							</Link>
-						</DropdownMenuItem>
-					);
-				})}
+						);
+					})}
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>
+						<SignOutLink />
+					</DropdownMenuItem>
+				</SignedIn>
 			</DropdownMenuContent>
 		</DropdownMenu>
 	);
--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,5 +1,17 @@
-function UserIcon() {
-	return <div>UserIcon</div>;
+import {currentUser} from "@clerk/nextjs/server";
+import {LuUser} from 'react-icons/lu';
+
+async function UserIcon() {
+	const user = await currentUser();
+	const profileImage = user?.imageUrl;
+	if (profileImage) {
+		return (
+			// eslint-disable-next-line @next/next/no-img-element
+			<img src={profileImage} alt="profile image" className="w-6 h-6 rounded-full object-cover" />
+		);
+	}
+
+	return <LuUser className="w-6 h-6 bg-primary rounded-full text-white" />
 }
 
 export default UserIcon;
