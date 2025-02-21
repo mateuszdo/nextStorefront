5dde3641631135efcfc3911988a647b974263b23
mateusz <dobrzynskimateusz@gmail.com>
2025-02-21 09:22:46 +0000

    remove delete product functionality for vercel compaint

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,7 +1,7 @@
 import EmptyList from "@/components/global/EmptyList";
-import {deleteProductAction, fetchAdminProducts} from "@/utils/actions";
+import {fetchAdminProducts} from "@/utils/actions";
 import Link from "next/link";
-import {IconButton} from "@/components/form/Buttons";
+
 import {formatCurrency} from "@/utils/format";
 import {
 	Table,
@@ -12,7 +12,6 @@ import {
 	TableHeader,
 	TableRow,
 } from "@/components/ui/table";
-import FormContainer from "@/components/form/FormContainer";
 
 async function AdminProductsPage() {
 	const items = await fetchAdminProducts();
@@ -21,7 +20,7 @@ async function AdminProductsPage() {
 		<section>
 			<Table>
 				<TableCaption className="capitalize">
-					total products: {items.length}
+					total products : {items.length}
 				</TableCaption>
 				<TableHeader>
 					<TableRow>
@@ -46,26 +45,14 @@ async function AdminProductsPage() {
 								</TableCell>
 								<TableCell>{company}</TableCell>
 								<TableCell>{formatCurrency(price)}</TableCell>
-								<TableCell className="flex items-center gap-x-2">
-									<Link href={`/admin/products/${productId}/edit`}>
-										<IconButton actionType="edit" />
-									</Link>
-									<DeleteProduct productId={productId} />
-								</TableCell>
+
+								<TableCell className="flex items-center gap-x-2"></TableCell>
 							</TableRow>
 						);
 					})}
 				</TableBody>
 			</Table>
 		</section>
-	);
-}
-function DeleteProduct({productId}: {productId: string}) {
-	const deleteProduct = deleteProductAction.bind(null, {productId});
-	return (
-		<FormContainer action={deleteProduct}>
-			<IconButton actionType="delete" />
-		</FormContainer>
 	);
 }
 
